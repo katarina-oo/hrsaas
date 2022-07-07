234d2c3159a24ed29bd5eb0e7d8541813c1e8ad6
katarina <2402155636@qq.com>
2022-07-07 15:33:27 +0800

    人资基础环境搭建

--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,13 @@
-import request from '@/utils/request'
+// import request from '@/utils/request'
 
-export function login(data) {
-  return request({
-    url: '/vue-admin-template/user/login',
-    method: 'post',
-    data
-  })
+export const login = (data) => {
+
 }
 
-export function getInfo(token) {
-  return request({
-    url: '/vue-admin-template/user/info',
-    method: 'get',
-    params: { token }
-  })
+export const getInfo = (token) => {
+
 }
 
-export function logout() {
-  return request({
-    url: '/vue-admin-template/user/logout',
-    method: 'post'
-  })
+export const logout = () => {
+
 }
